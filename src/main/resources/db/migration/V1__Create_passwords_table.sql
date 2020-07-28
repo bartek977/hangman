1fd4e8695211ba7ca838d96b989b73edd2cbb7e8
Bartekk <bartekturkosz@gmail.com>
2020-07-28 15:52:55 +0200

    game in js

---
+++ src/main/resources/db/migration/V1__Create_passwords_table.sql
@@ -0,0 +1,4 @@
+create table passwords (
+    id int unsigned primary key auto_increment,
+    password varchar(250) not null
+);
---
+++ src/main/resources/static/js/game.js
@@ -0,0 +1,81 @@
+var ALPHABET = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŻŹ";
+var password = "test";
+password = password.toUpperCase();
+var passwordLength = password.length;
+var passwordGame = "";
+var counter = 0;
+
+for(i=0; i<passwordLength; i++) {
+    if(password.charAt(i)==' ') {
+        passwordGame += " ";
+    }
+    else {
+        passwordGame += "-";
+    }
+}
+
+function write_password() {
+    document.getElementById("password").innerHTML = passwordGame;
+}
+
+window.onload = start;
+
+function start() {
+
+    var letters = "";
+
+    for(i=0; i<35; i++) {
+        var element = "letter" + i;
+        letters += '<div class="letter" onclick="check('+i+')" id="' + element + '">' + ALPHABET.charAt(i) + '</div>';
+        if((i+1)%7==0) {
+            letters += '<div style="clear:both;"></div>';
+        }
+    }
+
+    document.getElementById("alphabet").innerHTML = letters;
+
+    write_password();
+}
+
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+}
+
+function check(nr) {
+
+    var isLetterInPassword = false;
+
+    for(i=0; i<passwordLength; i++) {
+        if(password.charAt(i)==ALPHABET.charAt(nr)) {
+            passwordGame = passwordGame.replaceAt(i, ALPHABET.charAt(nr));
+            isLetterInPassword = true;
+
+        }
+    }
+
+    if(isLetterInPassword) {
+        write_password();
+        var element = "letter" + nr;
+        document.getElementById(element).style.background = "#003300";
+        document.getElementById(element).style.color = "#00C000";
+        document.getElementById(element).style.border = "3px solid #00C000";
+        document.getElementById(element).style.cursor = "default";
+
+    }
+    else {
+        counter++;
+        document.getElementById("game").innerHTML = '<img src="img/' + counter + '.jpg" />'
+        var element = "letter" + nr;
+        document.getElementById(element).style.background = "#330000";
+        document.getElementById(element).style.color = "#C00000";
+        document.getElementById(element).style.border = "3px solid #C00000";
+        document.getElementById(element).style.cursor = "default";
+        document.getElementById(element).setAttribute("onclick", ";");
+    }
+
+    if(counter>8) {
+        document.getElementById("alphabet").innerHTML  = "Przegrana! Prawidłowe hasło: "+password+'<br /><br /><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>';
+    }
+    if(passwordGame == password) {
+        document.getElementById("alphabet").innerHTML  = "Tak jest! Podano prawidłowe hasło: "+password+'<br /><br /><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>';    }
+}
